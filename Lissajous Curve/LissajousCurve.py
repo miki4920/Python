63047f6a0b0369cbcfb0e3bf3625178355a954a1
Mikolaj Grobelny <miki4920@gmail.com>
2019-04-05 09:46:53 +0100

    Lissajous Curve - variable sorted

--- Lissajous Curve/LissajousCurve.py
+++ Lissajous Curve/LissajousCurve.py
@@ -1,31 +1,35 @@
 import turtle
 import math
-import time
 screen = turtle.Screen()
 screen.bgcolor("white")
 Jonathan = turtle.Turtle()
 Jonathan.color("black")
 Jonathan.speed(0)
 
-def x_coordinate(a, delta):
+
+def x_coordinate(a, delta, steps, step_size):
     coordinate_list = []
-    for i in range(0, 1000000):
-        coordinate = 200*math.sin(a*i*0.01 + delta*0.01)
+    for step in range(0, steps):
+        coordinate = 200*math.sin(step_size*(a*step + delta))
         coordinate_list.append(coordinate)
     return coordinate_list
 
-def y_coordinate(b):
+
+def y_coordinate(b, steps, step_size):
     coordinate_list = []
-    for i in range(0, 1000000):
-        coordinate = 200*math.sin(i*b*0.01)
+    for step in range(0, steps):
+        coordinate = 200*math.sin(step*b*step_size)
         coordinate_list.append(coordinate)
     return coordinate_list
 
+
+steps = 1000000
+step_size = 0.001
 a = 56
 b = 55
 delta = math.pi/4
-x = x_coordinate(a, delta)
-y = y_coordinate(b)
+x = x_coordinate(a, delta, steps, step_size)
+y = y_coordinate(b, steps, step_size)
 for i in range(0, 1000000):
     Jonathan.goto(x[i], y[i])
-time.sleep(100)
+
